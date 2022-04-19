276ff25ce4cb80d04dbd50d5c13c2beead3ced16
noesis.stenorio <sara.g.tenorio@noesis.pt>
2022-04-19 14:29:58 +0100

    [with-hooks] INIT

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import ProductList from './components/ProductList';
+import FuncComp from './classVSFunc/FuncComp';
+import ClassComp from './classVSFunc/ClassComp';
 
 function App() {
   return (
     <div className="App">
-      <ProductList />
+      {/* <ProductList /> */}
+      <ClassComp />
+      <hr />
+      <FuncComp />
     </div>
   );
 }
