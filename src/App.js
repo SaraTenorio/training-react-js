efe7985cab27791e538f7bec6e2bc9717de15803
noesis.stenorio <sara.g.tenorio@noesis.pt>
2022-04-19 14:38:07 +0100

    [with-hooks] Part 02

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import logo from './logo.svg';
+/* AULA 01 */
+// import logo from './logo.svg';
+// import './App.css';
+// import ProductList from './components/ProductList';
+// import FuncComp from './classVSFunc/FuncComp';
+// import ClassComp from './classVSFunc/ClassComp';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <ProductList /> */}
+//       <ClassComp />
+//       <hr />
+//       <FuncComp />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/* AULA 02 */
 import './App.css';
-import ProductList from './components/ProductList';
-import FuncComp from './classVSFunc/FuncComp';
-import ClassComp from './classVSFunc/ClassComp';
+import Bookables from './components/Bookables';
+import Lampada from './lampada/Lampada';
 
 function App() {
   return (
     <div className="App">
-      {/* <ProductList /> */}
-      <ClassComp />
-      <hr />
-      <FuncComp />
+      {/* <Bookables /> */}
+      <Lampada />
     </div>
   );
 }
---
+++ src/lampada/Lampada.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Light from './LightBulbSvg'
+
+export default function Lampada() {
+    const [ligth, setLigth] = useState(false)
+    const fillColor = ligth ? "gold": "gray";
+
+    return (
+        <>
+        <div>
+            <Light fillColor={fillColor} />
+            <hr />
+            <button onClick={ () => setLigth(!ligth) }>ON/OFF</button>
+        </div>        
+        </>
+    )
+}
